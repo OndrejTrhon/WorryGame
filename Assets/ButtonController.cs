1b93150c6987324058b516fea15f2a58bd4fc8b2
Ondřej Trhoň <ondrej@trhon.cz>
2020-05-09 10:35:47 +0200

    CameraFollow, Button/Dialog alphascript

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject DialogBubbleObject;
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) {
+            Destroy(gameObject);
+            Instantiate(DialogBubbleObject, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        }
+    }
+}
--- Assets/NPCController.cs
+++ Assets/NPCController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class NPCController : MonoBehaviour
 {
-    public GameObject DialogObject;
+    public GameObject ButtonObject;
     static bool ColliderLock;
 
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D col)
     {
         if (!ColliderLock) { 
-         Instantiate(DialogObject, new Vector3(transform.position.x, transform.position.y+2, 0), Quaternion.identity);
+         Instantiate(ButtonObject, new Vector3(transform.position.x, transform.position.y+2, 0), Quaternion.identity);
         Debug.Log("Instantiated");
             LockColliderLock();
         }
