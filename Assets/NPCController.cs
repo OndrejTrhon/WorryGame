810fd273c8f0dc675c93821aeadf9df2c5983248
Ondřej Trhoň <ondrej@trhon.cz>
2020-05-31 16:30:14 +0200

    Incorrectly disabled speech bubbles fix

--- Assets/NPCController.cs
+++ Assets/NPCController.cs
@@ -7,6 +7,7 @@ public class NPCController : MonoBehaviour
     public GameObject SpeechObject;
     public GameObject PopUpObject;
     private GameObject InstantiatedSpeechObject;
+    private GameObject SelfReference;
     static bool ColliderLock;
     private bool IsOnCollision = false;
     private bool BubbleInstantiated = false;
@@ -16,17 +17,29 @@ public class NPCController : MonoBehaviour
     {
         if (IsOnCollision == true && Input.GetKeyDown(KeyCode.Space) && BubbleInstantiated == false)
         {
-            InstantiatedSpeechObject.SetActive(false);
             Instantiate(PopUpObject, new Vector3(transform.position.x, transform.position.y + 3f, 0), Quaternion.identity);
             SceneController.WorryCount++;
             BubbleInstantiated = true;
+        }
+
+        if (BubbleInstantiated == true)
+        {
+            int VarChildCount = SelfReference.transform.childCount;
+            for (int i = 0; i < VarChildCount; i++)
+            {
+                var child = SelfReference.transform.GetChild(i).gameObject;
+                if (child != null)
+                    child.SetActive(false);
+            }
         }
     }
 
     private void Start()
     {
+        SelfReference = this.gameObject;
         Invoke("InstantiateSpeech", Random.Range(1.0f, 10f));
         Invoke("InstantiateSpeech", Random.Range(1.0f, 10f));
+        
     }
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -41,6 +54,7 @@ public class NPCController : MonoBehaviour
     void InstantiateSpeech()
     {
         InstantiatedSpeechObject = Instantiate(SpeechObject, new Vector3(transform.position.x, transform.position.y + 2, 0), Quaternion.identity);
+        InstantiatedSpeechObject.transform.SetParent(this.gameObject.transform, true);
     }
 
 }
